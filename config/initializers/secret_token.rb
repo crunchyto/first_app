efc5df94ca9f4e3d1503dddb6c7e8bea55585614
Crunchyto <projas@projas-virtual-machine.(none)>
2012-10-23 18:59:31 +0200

    subida inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '9bd515ebdea33e9f9655bb83c4196ce5b851f0c56459c2839e94a7b51da1149761764606f21568ff78419e206714335ee34009f438275157990180fd05d023fd'
